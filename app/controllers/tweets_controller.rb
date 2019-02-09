2cfd9d78eb7b491747f850ec0cb34232d684dd08
Heather Miller <heather.miller114@gmail.com>
2019-02-09 18:12:16 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,10 +11,26 @@ class TweetsController < ApplicationController
     
     get '/tweets/new' do
         #load create tweet form
+        if logged_in?
+            erb :'tweets/new'
+        else
+            redirect '/login'
+        end
     end
 
     post '/tweets' do
         #process new form submission
+        #binding.pry
+        if params[:content].empty?
+            redirect "/tweets/new"
+        else
+            @tweet = Tweet.create(params)
+            @tweet.user_id = current_user.id
+            @tweet.save
+
+            redirect "/tweets/#{@tweet.id}"
+        end
+    
     end
 
     get '/tweets/:id' do
